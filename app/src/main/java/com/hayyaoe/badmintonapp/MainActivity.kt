442e5f568a3bc67ed36bc89885feccf734076e9a
derendmarvel <derend101@gmail.com>
2023-12-13 13:56:00 +0700

    Home and Settings View

--- app/src/main/java/com/hayyaoe/badmintonapp/MainActivity.kt
+++ app/src/main/java/com/hayyaoe/badmintonapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hayyaoe.badmintonapp.ui.theme.BadmintonAppTheme
+import com.hayyaoe.badmintonapp.ui.view.HomeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    HomeView()
                 }
             }
         }
